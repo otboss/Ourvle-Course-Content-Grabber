4ddcf49293794adce68d86c9913d4bc719528c31
Ottor <otsurfer6@gmail.com>
2022-04-29 01:06:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -23,5 +23,5 @@ for(let resourceIcon of resourceIcons){
         resourceIcon.parentElement.setAttribute("download", true)
         resourceIcon.parentElement.click()
     }
-    await new Promise((resolve) => setTimeout(resolve, 100))
+    await new Promise(resolve => setTimeout(resolve, 100))
 }

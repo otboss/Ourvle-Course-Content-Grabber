917d72ad44aad1107ddd0b01b213a2308e2d1529
Ottor <otsurfer6@gmail.com>
2022-04-28 21:28:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,27 +2,22 @@
  * BY: OTTOR
  *      09/12/2017
  * 
- * ON THE DESIRED COURSE PAGE COPY THE SCRIPT PROVIDE IN THE BROWSER CONSOLE
- * WORKS WITH GOOGLE CHROME, FOR BEST RESULTS DISABLE SAVE DESTINATION PROMPT
+ * ON GOOGLE CHROME, GO TO THE DESIRED COURSE CONTAINER COPY THE SCRIPT PROVIDE IN THE BROWSER CONSOLE
+ * **** FOR BEST RESULTS DISABLE SAVE DESTINATION PROMPT ****
  * */
 
-    window.setTimeout(function(){
-        var usefulFiles = [
-            "pdf",
-            "archive",
-            "powerpoint",
-            "spreadsheet",
-            "document"
-        ];
-        document.querySelectorAll("a > img").forEach((resourceIcon) => {
-            var picLink = resourceIcon["src"];
-            for(var x = 0; x < usefulFiles.length; x++){
-                if(picLink.indexOf(usefulFiles[x]) >= 0){
-                    resourceIcon.parentElement.setAttribute("download", true)
-                    resourceIcon.parentElement.click();
-                    break;
-                }
-            }            
-            
-        });
-    }, 2000);
+const resourceIcons = document.querySelectorAll("img")
+for(let resourceIcon of resourceIcons){
+    var picLink = resourceIcon["src"];
+    if(
+        picLink.indexOf("pdf") >= 0
+        || picLink.indexOf("spreadsheet") >= 0
+        || picLink.indexOf("archive") >= 0
+        || picLink.indexOf("powerpoint") >= 0
+        || picLink.indexOf("document") >= 0
+    ){
+        resourceIcon.parentElement.setAttribute("download", true)
+        resourceIcon.parentElement.click();
+    }
+    await new Promise((resolve) => setTimeout(resolve, 100))
+}

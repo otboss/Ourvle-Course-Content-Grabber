687b450e76a33662bf6a69f2544be10840556571
Ottor <otsurfer6@gmail.com>
2022-04-28 21:10:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,28 +5,23 @@
  * ON THE DESIRED COURSE PAGE COPY THE SCRIPT PROVIDE IN THE BROWSER CONSOLE
  * WORKS WITH GOOGLE CHROME, FOR BEST RESULTS DISABLE SAVE DESTINATION PROMPT
  * */
-    
-    var jq = document.createElement('script');
-    jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
-    document.getElementsByTagName('head')[0].appendChild(jq);
 
     window.setTimeout(function(){
-        $("section a").each(function(){
-            $(this).attr("download", true);
-        });  
         var usefulFiles = [
             "pdf",
             "archive",
             "powerpoint",
             "document"
         ];
-        $("section a img").each(function(){
-            var picLink = $(this).attr("src");
+        document.querySelectorAll("a > img").forEach((resourceIcon) => {
+            var picLink = resourceIcon["src"];
             for(var x = 0; x < usefulFiles.length; x++){
-                if(picLink.search(usefulFiles[x]) >= 0){
-                    this.click();
+                if(picLink.indexOf(usefulFiles[x]) >= 0){
+                    resourceIcon.parentElement.setAttribute("download", true)
+                    resourceIcon.parentElement.click();
                     break;
                 }
-            }
+            }            
+            
         });
     }, 2000);
